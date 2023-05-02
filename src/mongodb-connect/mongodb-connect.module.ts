4c802888b3eab55769eaa8692340db37cf394c6b
albertmata <albertmata@pop-os.localdomain>
2023-05-02 08:39:18 -0600

    base dev

--- src/mongodb-connect/mongodb-connect.module.ts
+++ src/mongodb-connect/mongodb-connect.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose';
 
+const prodURL:string ='mongodb://u6pdhvoipuyt36xoiosj:571yTiCu2NDBwxCCrnrU@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/b6c8fducycmfd3c?replicaSet=rs0'
+const devURL:string='mongodb://0.0.0.0:27017/DWYT'
+
+const url= prodURL;
 @Module({
     imports:[
-    MongooseModule.forRoot('mongodb://u6pdhvoipuyt36xoiosj:571yTiCu2NDBwxCCrnrU@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/b6c8fducycmfd3c?replicaSet=rs0')
+    MongooseModule.forRoot(url)
     ]
 })
 export class MongodbConnectModule {}
